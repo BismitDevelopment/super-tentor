3d332e4e781fb97a50ad2a502c0390a2ae04f25e
hanifaudah <hanifarkanaudah@gmail.com>
2020-03-19 13:22:33 +0700

    Stylized Login and Register Pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,188 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    {{-- Bootstrap --}}
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    {{-- Font --}}
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,800&display=swap" rel="stylesheet">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    {{-- Script --}}
+    <script
+        src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous">
+    </script>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            width: 100vw;
+            font-family: 'Montserrat';
+        }
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+        form {
+            width: 20em;
+        }
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+        form * {
+            margin: .5em 0;
+        }
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+        input {
+            border-radius: 10px;
+            border: 1px solid rgba(0, 0, 0, 0.25);
+            width: 100%;
+            padding: .7em;
+            outline: none;
+        }
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+        input::placeholder {
+            color: rgba(196, 196, 196, .9);
+        }
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+        input[type=text] {
+            background-image: url(/img/profile-photo.svg);
+            background-repeat:no-repeat;
+            background-size: 1.3em;
+            background-position: 4% 50%;
+            padding-left: 2.5em;
+        }
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+        input[type=password] {
+            background-image: url(/img/lock.svg);
+            background-repeat:no-repeat;
+            background-size: 1.3em;
+            background-position: 4% 50%;
+            padding-left: 2.5em;
+        }
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
+        h1 {
+            font-weight: 700;
+            font-size: 2.5em;
+        }
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+        h5, a {
+            font-weight: normal;
+            color: rgba(0, 0, 0, 0.3);
+            text-align: center;
+            font-size: .9em;
+        }
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+        a {
+            color: #67A69D;
+        }
+
+        a + div * {
+            margin: 0;
+            padding: 0;
+            vertical-align: text-bottom;
+        }
+
+        button {
+            background: #E9D038;
+            border-radius: 10px;
+            outline: none;
+            border: none;
+            padding: .5em 0;
+            width: 100%;
+            color: white;
+            font-weight: 800;
+            font-size: 1.3em;
+        }
+
+        .login-body {
+            background: white;
+            border-radius: 20px;
+            padding: 3em 5em;
+        }
+
+        .line {
+            border: .5px solid #C4C4C4;
+            width: 8em;
+            height: 1px;
+        }
+
+        .line + h4 {
+            font-size: 1.2em;
+            color: #C4C4C4;
+            margin: 0 .5em;
+        }
+
+        @media (max-width: 576px) {
+            form {
+                width: 60vw;
+            }
+
+            .login-body {
+                width: 80vw;
+                font-size: 4vw;
+            }
+
+            .line {
+                width: 25vw;
+            }
+
+            #facebook-btn {
+                width: 60vw;
+            }
+        }
+
+    </style>
+</head>
+<body>
+    <div class="login-body d-flex flex-column align-items-center">
+        <h1>Login</h1>
+        <form class="d-flex flex-column align-items-center" action="/login" method="POST">
+            @csrf
+            <input type="text" name="username" placeholder="Username">
+            <input id="password" type="password" placeholder="Password" class="@error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+            @if (Route::has('password.request'))
+                <a href="{{ route('password.request') }}">
+                {{ __('Lupa Kata Sandi?') }}
+                </a>
+            @endif
+            <button type="submit" class="">{{ __('Login') }}</button>
+            <div class="d-flex flex-row align-items-center justify-content-center">
+                <span class="line"></span><h4>atau</h4><span class="line"></span>
             </div>
-        </div>
+            <a href="" id="facebook-btn"><img class="img-fluid" src="/img/facebook-login.svg" alt=""></a>
+            <div class="d-flex justify-content-center">
+                <h5>Belum punya akun?</h5>
+                <a href="/register">Daftar di sini</a>
+            </div>
+        </form>
     </div>
-</div>
-@endsection
+
+    <script>
+        $(document).mousemove(function(event) {
+            windowWidth = $(window).width();
+            windowHeight = $(window).height();
+            
+            mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
+            mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
+        
+            $('body').css(
+                'background', 
+                `
+                radial-gradient(circle at ` + mouseXpercentage + `% ` + mouseYpercentage + `%, rgba(233, 208, 56, 1), rgba(233, 208, 56, 1) 5%, rgba(103, 166, 157, .2) 10%, rgba(0,0,0,0) 10%),
+                radial-gradient(circle at ` + mouseXpercentage + `% ` + mouseYpercentage + `%, rgba(233, 208, 56, 1), rgba(233, 208, 56, 1) 10%, rgba(103, 166, 157, .2) 20%, rgba(0,0,0,0) 10%),
+                radial-gradient(circle at ` + mouseXpercentage + `% ` + mouseYpercentage + `%, rgba(233, 208, 56, 1), rgba(233, 208, 56, 1) 10%, rgba(103, 166, 157, .2) 30%, rgba(0,0,0,0) 10%),
+                radial-gradient(circle at ` + mouseXpercentage + `% ` + mouseYpercentage + `%, rgba(233, 208, 56, 1), rgba(233, 208, 56, 1) 20%, rgba(103, 166, 157, .2) 40%, rgba(0,0,0,0) 20%),
+                radial-gradient(circle at ` + mouseXpercentage + `% ` + mouseYpercentage + `%, rgba(233, 208, 56, 1), rgba(233, 208, 56, 1) 10%, rgba(103, 166, 157, .2) 50%, rgba(0,0,0,0) 30%),
+                radial-gradient(circle at ` + mouseXpercentage + `% ` + mouseYpercentage + `%, rgba(233, 208, 56, 1), rgba(233, 208, 56, 1) 30%, rgba(103, 166, 157, .2) 60%, rgba(0,0,0,0) 40%),
+                radial-gradient(circle at ` + mouseXpercentage + `% ` + mouseYpercentage + `%, rgba(233, 208, 56, 1), rgba(233, 208, 56, 1) 40%, rgba(103, 166, 157, .2) 80%, rgba(0,0,0,0) 50%),
+                radial-gradient(circle at ` + mouseXpercentage + `% ` + mouseYpercentage + `%, rgba(233, 208, 56, .5), rgba(233, 208, 56, .5) 50%, rgba(103, 166, 157, .2) 90%, rgba(0,0,0,0) 75%),
+                radial-gradient(circle at ` + mouseXpercentage + `% ` + mouseYpercentage + `%, rgba(233, 208, 56, .5), rgba(233, 208, 56, 1) 60%, rgba(103, 166, 157, 1) 100%, rgba(0,0,0,0) 75%)
+                `);
+        });
+    </script>
+</body>
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,207 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Daftar</title>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    {{-- Bootstrap --}}
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+    {{-- Font --}}
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,800&display=swap" rel="stylesheet">
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+    {{-- Script --}}
+    <script
+        src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous">
+    </script>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            width: 100vw;
+            font-family: 'Montserrat';
+        }
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+        form {
+            width: 20em;
+        }
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+        form * {
+            margin: .5em 0;
+        }
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+        input {
+            border-radius: 10px;
+            border: 1px solid rgba(0, 0, 0, 0.25);
+            width: 100%;
+            padding: .7em;
+            outline: none;
+        }
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+        input::placeholder {
+            color: rgba(196, 196, 196, .9);
+        }
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+        input[name=name] {
+            background-image: url(/img/smile.svg);
+            background-repeat:no-repeat;
+            background-size: 1.3em;
+            background-position: 4% 50%;
+            padding-left: 2.5em;
+        }
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+        input[name=username] {
+            background-image: url(/img/profile-photo.svg);
+            background-repeat:no-repeat;
+            background-size: 1.3em;
+            background-position: 4% 50%;
+            padding-left: 2.5em;
+        }
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+        input[type=password] {
+            background-image: url(/img/lock.svg);
+            background-repeat:no-repeat;
+            background-size: 1.3em;
+            background-position: 4% 50%;
+            padding-left: 2.5em;
+        }
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+        input[name=email] {
+            background-image: url(/img/email.svg);
+            background-repeat:no-repeat;
+            background-size: 1.3em;
+            background-position: 4% 50%;
+            padding-left: 2.5em;
+        }
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+        h1 {
+            font-weight: 700;
+            font-size: 2.5em;
+        }
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
+        h5, a {
+            font-weight: normal;
+            color: rgba(0, 0, 0, 0.3);
+            text-align: center;
+            font-size: .9em;
+        }
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+        a {
+            color: #67A69D;
+        }
+
+        a + div * {
+            margin: 0;
+            padding: 0;
+            vertical-align: text-bottom;
+        }
+
+        button {
+            background: #E9D038;
+            border-radius: 10px;
+            outline: none;
+            border: none;
+            padding: .5em 0;
+            width: 100%;
+            color: white;
+            font-weight: 800;
+            font-size: 1.3em;
+        }
+
+        .login-body {
+            background: white;
+            border-radius: 20px;
+            padding: 3em 5em;
+        }
+
+        .line {
+            border: .5px solid #C4C4C4;
+            width: 8em;
+            height: 1px;
+        }
+
+        .line + h4 {
+            font-size: 1.2em;
+            color: #C4C4C4;
+            margin: 0 .5em;
+        }
+
+        @media (max-width: 576px) {
+            form {
+                width: 60vw;
+            }
+
+            .login-body {
+                width: 80vw;
+                font-size: 4vw;
+            }
+
+            .line {
+                width: 25vw;
+            }
+
+            #facebook-btn {
+                width: 60vw;
+            }
+        }
+
+    </style>
+</head>
+<body>
+    <div class="login-body d-flex flex-column align-items-center">
+        <h1>Daftar</h1>
+        <form class="d-flex flex-column align-items-center" action="/register" method="POST">
+            @csrf
+            <input id="name" placeholder="Nama" type="text" class="@error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+            @error('name')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+            <input id="password" type="password" placeholder="Password" class="@error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+            <input type="text" name="username" placeholder="Username">
+            <input id="email" placeholder="email" type="email" class="@error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+            @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+            <button type="submit" class="">{{ __('Daftar') }}</button>
+            <div class="d-flex flex-row align-items-center justify-content-center">
+                <span class="line"></span><h4>atau</h4><span class="line"></span>
             </div>
-        </div>
+            <a href="" id="facebook-btn"><img class="img-fluid" src="/img/facebook-login.svg" alt=""></a>
+        </form>
     </div>
-</div>
-@endsection
+
+    <script>
+        $(document).mousemove(function(event) {
+            windowWidth = $(window).width();
+            windowHeight = $(window).height();
+            
+            mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
+            mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
+        
+            $('body').css(
+                'background', 
+                `
+                radial-gradient(circle at ` + mouseXpercentage + `% ` + mouseYpercentage + `%, rgba(233, 208, 56, 1), rgba(233, 208, 56, 1) 5%, rgba(103, 166, 157, .2) 10%, rgba(0,0,0,0) 10%),
+                radial-gradient(circle at ` + mouseXpercentage + `% ` + mouseYpercentage + `%, rgba(233, 208, 56, 1), rgba(233, 208, 56, 1) 10%, rgba(103, 166, 157, .2) 20%, rgba(0,0,0,0) 10%),
+                radial-gradient(circle at ` + mouseXpercentage + `% ` + mouseYpercentage + `%, rgba(233, 208, 56, 1), rgba(233, 208, 56, 1) 10%, rgba(103, 166, 157, .2) 30%, rgba(0,0,0,0) 10%),
+                radial-gradient(circle at ` + mouseXpercentage + `% ` + mouseYpercentage + `%, rgba(233, 208, 56, 1), rgba(233, 208, 56, 1) 20%, rgba(103, 166, 157, .2) 40%, rgba(0,0,0,0) 20%),
+                radial-gradient(circle at ` + mouseXpercentage + `% ` + mouseYpercentage + `%, rgba(233, 208, 56, 1), rgba(233, 208, 56, 1) 10%, rgba(103, 166, 157, .2) 50%, rgba(0,0,0,0) 30%),
+                radial-gradient(circle at ` + mouseXpercentage + `% ` + mouseYpercentage + `%, rgba(233, 208, 56, 1), rgba(233, 208, 56, 1) 30%, rgba(103, 166, 157, .2) 60%, rgba(0,0,0,0) 40%),
+                radial-gradient(circle at ` + mouseXpercentage + `% ` + mouseYpercentage + `%, rgba(233, 208, 56, 1), rgba(233, 208, 56, 1) 40%, rgba(103, 166, 157, .2) 80%, rgba(0,0,0,0) 50%),
+                radial-gradient(circle at ` + mouseXpercentage + `% ` + mouseYpercentage + `%, rgba(233, 208, 56, .5), rgba(233, 208, 56, .5) 50%, rgba(103, 166, 157, .2) 90%, rgba(0,0,0,0) 75%),
+                radial-gradient(circle at ` + mouseXpercentage + `% ` + mouseYpercentage + `%, rgba(233, 208, 56, .5), rgba(233, 208, 56, 1) 60%, rgba(103, 166, 157, 1) 100%, rgba(0,0,0,0) 75%)
+                `);
+        });
+    </script>
+</body>
+</html>
