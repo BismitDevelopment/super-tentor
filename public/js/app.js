8636b959a84dbd8cecde66a3de5a499439014ede
ganiirsyadi <ganiilhamirsyadi@gmail.com>
2020-03-20 21:42:49 +0700

    Adding navbar

--- public/js/app.js
+++ public/js/app.js
@@ -49654,15 +49654,27 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/sass/navbar.scss":
+/*!************************************!*\
+  !*** ./resources/sass/navbar.scss ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!******************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/navbar.scss ***!
+  \******************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /run/media/zuhdi/WIN_D/Backup_Zuhdi/Work/BEM/Bismit_2020/Projects/super-tentor/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /run/media/zuhdi/WIN_D/Backup_Zuhdi/Work/BEM/Bismit_2020/Projects/super-tentor/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\bismit\Project\super-tentor\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! D:\bismit\Project\super-tentor\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! D:\bismit\Project\super-tentor\resources\sass\navbar.scss */"./resources/sass/navbar.scss");
 
 
 /***/ })
