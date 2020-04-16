a08f3a0d3a9b5fc33335ab7ba6a04c71cfbc1b76
ganiirsyadi <ganiilhamirsyadi@gmail.com>
2020-04-16 21:57:37 +0700

    Fix Navbar landing and dashboard

--- resources/views/navbarDashboard.blade.php
+++ resources/views/navbarDashboard.blade.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="{{ asset('css/navbarDashboard.css') }}">
-<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-white shadow px-4">
+<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-white shadow px-5">
     <div class="container-fluid">
         <a class="navbar-brand py-1 mx-0" onclick="openNav()">
             <img src="{{ asset('img/BurgerMenu.svg') }}" height="25" class="">
@@ -7,7 +7,10 @@
         <a class="logo navbar-brand py-1 mx-auto" href="{{ route('welcome') }}">
             <img src="{{ asset('img/logo-nav.svg') }}" height="35" class="">
         </a>
-        <a class="my-auto mr-2 username">Pengguna 008</a>
+        <a href="">
+            <img src="{{ asset('img/mdi_notifications.svg') }}" alt="">
+        </a>
+        <a class="my-auto mx-3 username">Pengguna 008</a>
         <a class="py-0 my-0 profile-picture">
             <img class="" src="{{ asset('img/Profile.jpg') }}" alt="">
         </a>
